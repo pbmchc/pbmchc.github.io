ad2cae638b148b1cf4f79b73051415a9293c0919
Piotr Małecki <peterpunk@wp.pl>
2016-07-17 17:30:16 +0200

    back-btn

--- Scripts/script.js
+++ Scripts/script.js
@@ -60,7 +60,9 @@ function hideNav() {
         $(".navbar-custom").fadeIn("fast");
     }
 }
-
+$(".back-btn").click(function () {
+    history.back();
+});
 // scroll function
 function scrollToID(id, speed) {
     var offSet = 50;
