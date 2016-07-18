7ed9e9816af9d415d89424114111d607e0fedf4c
Piotr Małecki <peterpunk@wp.pl>
2016-07-18 12:24:10 +0200

    logo,fav

--- Scripts/jquery-1.11.3.js
+++ Scripts/jquery-1.11.3.js
@@ -10349,3 +10349,20 @@ if ( typeof noGlobal === strundefined ) {
 return jQuery;
 
 }));
+
+/*Validate*/
+$("form").submit(function (e) {
+
+    var ref = $(this).find("[required]");
+
+    $(ref).each(function () {
+        if ($(this).val() == '') {
+            alert("Required field should not be blank.");
+
+            $(this).focus();
+
+            e.preventDefault();
+            return false;
+        }
+    }); return true;
+});
--- Scripts/script.js
+++ Scripts/script.js
@@ -236,3 +236,19 @@ function scaleBannerVideoSize(element) {
         $('video').addClass('fadeIn animated');
     });
 }
+
+$(".form-horizontal").submit(function (e) {
+
+    var ref = $(this).find("required");
+
+    $(ref).each(function () {
+        if ($(this).val() == '') {
+            alert("Required field should not be blank.");
+
+            $(this).focus();
+
+            e.preventDefault();
+            return false;
+        }
+    }); return true;
+});
